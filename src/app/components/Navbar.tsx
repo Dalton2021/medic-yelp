10dacd39baa5999bc3c408bdb7af3d15b97920f2
Dalton2021 <dabner@clutch-inc.com>
2025-08-25 15:49:50 -0400

    navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,26 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="p-4">
-      <div className="bg-[url('https://www.ratemyprofessors.com/assets/bkgd-blob-yellow-CkfZZZSs.svg')] bg-no-repeat bg-right-bottom">
-        <div className="grid grid-cols-6 text-center">
-          <div className="col-start-5 flex justify-end items-center text-lg">
-            <Link href="/login" className="font-semibold pe-5">
-              Log In
-            </Link>
-            <Button className="rounded-full font-bold px-6">Sign Up</Button>
+    <nav className="h-16">
+      <div className="h-full p-4">
+        <div className="grid grid-cols-6 text-center h-full">
+          <div className="col-start-5 flex justify-end items-center text-lg relative">
+            <Image
+              src="https://www.ratemyprofessors.com/assets/bkgd-blob-yellow-CkfZZZSs.svg"
+              alt="blob"
+              width={250}
+              height={100}
+              className="absolute z-0 object-contain"
+            />
+            <div className="z-10 px-5">
+              <Link href="/login" className="font-semibold pe-5">
+                Log In
+              </Link>
+              <Button className="rounded-full font-bold px-6">Sign Up</Button>
+            </div>
           </div>
         </div>
       </div>
