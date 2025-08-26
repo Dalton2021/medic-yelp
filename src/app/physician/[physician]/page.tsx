cedbe0045389343428c005a3b769efce7f98a8ce
Mary Brennan <mbrennan@clutch-inc.com>
2025-08-25 17:03:09 -0700

    modernizing website using shadcn

--- src/app/physician/[physician]/page.tsx
+++ src/app/physician/[physician]/page.tsx
@@ -1,3 +1,4 @@
+import DoctorRatingChart from "@/components/DoctorRatingChart";
 import AppNavbar from "../../../components/appNavbar";
 import POverallStats from "@/components/POverallStats";
 export default function Page() {
@@ -5,8 +6,12 @@ export default function Page() {
     <div className="grid lg:grid-cols-6 gap-x-12">
       <div className=" col-start-2 col-span-3 gap-x-12">
         <div className="grid grid-cols-2 gap-x-12">
-          <POverallStats />
-          <div>yolo</div>
+          <div>
+            <POverallStats />
+            <div>More Deets</div>
+          </div>
+
+          <DoctorRatingChart />
         </div>
       </div>
     </div>
