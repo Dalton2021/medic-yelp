6a2e40cbd8e7addd6ff6b7aca2b79963652f7520
Dalton2021 <dabner@clutch-inc.com>
2025-08-28 14:08:58 -0400

    navbar start

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
-    <nav></nav>
+    <nav className="h-16 bg-black text-white">
+      <div className="h-full py-2 px-20">
+        <div className="grid grid-cols-12 h-full">
+          <div className="col-start-4 col-span-1 flex place-items-center">
+            <Link href="/">
+              <Image src="/rmp-icon.svg" alt="rmp" width={70} height={70} />
+            </Link>
+          </div>
+          <div className="col-span-1 flex place-items-center">tbd</div>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
